0541f3235a6b80bdcea0da50458beceff327e371
Roman <rome.gun@gmail.com>
2013-03-26 17:42:17 -0400

    Clear Functionality

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,4 @@
-Shoes.app :title => "Thy Calculator", :width => 200, :height => 210, :resizable => false do
+Shoes.app :title => "Thy Calculator", :width => 200, :height => 210, :resizable => false :color => blue do
   stack :margin => 20 do
     @output = edit_line :width => 150, :margin_bottom => 10
     
@@ -9,8 +9,12 @@ Shoes.app :title => "Thy Calculator", :width => 200, :height => 210, :resizable 
         end
       end
       
-      button "=", :width => 80, :height => 30 do 
+      button "=" do
         eval_expression
+      end
+
+      button "C" do
+        clear_all
       end
     end
     
@@ -30,6 +34,11 @@ Shoes.app :title => "Thy Calculator", :width => 200, :height => 210, :resizable 
   #
   def eval_expression
     @input = eval(@input).to_s
+    @output.text = @input
+  end
+
+  def clear_all
+    @input = ""
     @output.text = @input
   end
   
