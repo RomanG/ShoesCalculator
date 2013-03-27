67b9f4da17b12be731455012a7187ed3fe78588e
Roman <rome.gun@gmail.com>
2013-03-26 22:23:00 -0400

    Some Scientific Functions

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,4 @@
-Shoes.app :title => "Thy Calculator", :width => 200, :height => 210, :resizable => false  do background "#EBFFFF"
+Shoes.app :title => "Thy Calculator", :width => 200, :height => 240, :resizable => false  do background "#EBFFFF"
   stack :margin => 20 do
     @output = edit_line :width => 150, :margin_bottom => 10
 
@@ -16,9 +16,20 @@ Shoes.app :title => "Thy Calculator", :width => 200, :height => 210, :resizable 
       button "C" do
         clear_all
       end
+
+      button "^2" do
+        square
+        end
+
+      button "sqrt", :width => 72 do
+        square_root
+        end
+
+        button "^" do
+          power
+        end
     end
   end
-  
   # Stick a string on the end of our input
   #
   def append(s)
@@ -39,5 +50,17 @@ Shoes.app :title => "Thy Calculator", :width => 200, :height => 210, :resizable 
   def clear_all
     @input = ""
     @output.text = @input
+
+  def square
+      @input = @input.to_i * @input.to_i
+  end
+
+  def square_root
+    @input = Math.sqrt(@input.to_i)
+  end
+
+  def power
+    @input = @input**@input
+  end
   end
 end
